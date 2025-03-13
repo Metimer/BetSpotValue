eed39d73897be996d27b423224d2e3db6bb66fcf
Metimer <metinamerwane@gmail.com>
2025-03-13 14:05:16 +0100

    Update extract_advanced.py

--- extract_advanced.py
+++ extract_advanced.py
@@ -17,6 +17,8 @@ ligues = {
     'Allemagne': '20/Statistiques-Bundesliga',
     'Angleterre': '9/Statistiques-Premier-League'
 }
+chrome_options = Options()
+chrome_options.add_argument("--user-data-dir=/tmp/chrome_data")
 
 # Initialisation du service avec le gestionnaire de ChromeDriver
 service = Service(ChromeDriverManager().install())
